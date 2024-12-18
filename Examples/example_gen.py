e871c7f77a71a95018d9b4aa5fa5d8cf32f8c9bb
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2024-12-18 16:57:36 -0500

    Update example_gen.py

--- Examples/example_gen.py
+++ Examples/example_gen.py
@@ -2,7 +2,7 @@ from delta.generators.os_windows.comsvcs.comsvcs import (bundle__comsvcs_p0000)
 
 
 bundle__comsvcs = bundle__comsvcs_p0000()
-bundle_comsvcs_json =bundle__comsvcs.serialize()
+bundle_comsvcs_json = bundle__comsvcs.serialize()
 
 with open('bundle-comsvcs-delta.json', 'w') as file:
     file.write(bundle_comsvcs_json)
