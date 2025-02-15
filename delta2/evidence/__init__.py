8cb61994bffd6213407572132e0bbf87a6aad9c9
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2025-02-15 13:42:23 -0500

    update evidence

---
+++ delta2/evidence/__init__.py
@@ -0,0 +1,7 @@
+from .windows_any.comsvcs__evidence import (comsvcs__p0001___evidence,
+                                            comsvcs__p0002___evidence,
+                                            comsvcs__p0003___evidence
+                                            )
+from .windows_any.rundll__evidence import (rundll__p0001___evidence,
+                                           rundll__p0002___evidence,
+                                           rundll__p0003___evidence)
