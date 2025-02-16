867640a3cee9068bcf57c33aef3318e5814888ce
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2025-02-16 08:51:40 -0500

    update

--- delta2/patterns/__init__.py
+++ delta2/patterns/__init__.py
@@ -1,6 +1,6 @@
-from .windows_any.comsvcs__p0000 import (comsvcs__p0001___process_create__windows_any,
+from .win_lol_any.comsvcs__p0000 import (comsvcs__p0001___process_create__windows_any,
                                          comsvcs__p0002___process_create__windows_any__mde,
                                          comsvcs__p0003___read_process_memory__windows_any__mde)
-from .windows_any.rundll__p0000 import (rundll__p0001___process_create__windows_any,
+from .win_lol_any.rundll__p0000 import (rundll__p0001___process_create__windows_any,
                                         rundll__p0002___file_create__windows_any,
                                         rundll__p0003___read_process_memory__windows_any)
