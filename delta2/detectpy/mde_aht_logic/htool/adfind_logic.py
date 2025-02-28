bb3e300a2ccccc1207a51a38aa7ae4544dee4779
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2025-02-28 08:36:58 -0500

    Update adfind_logic.py

--- delta2/detectpy/mde_aht_logic/htool/adfind_logic.py
+++ delta2/detectpy/mde_aht_logic/htool/adfind_logic.py
@@ -8,7 +8,7 @@ def adfind_p0001(kql_ago='1d'):
         """
     query_json = {
         "delta": ["adfind-p0001--process_create-windows_any"],
-        "title": "Adfind Enumeration By ObjectCategory Filter by OU Command",
+        "title": "Adfind ObjectCategory Filter by OU Command",
         "mitre_technique": "t1003",
         "mitre_sub_technique": "t1003.001",
         "query": query_text
@@ -26,7 +26,7 @@ def adfind_p0002(kql_ago='1d'):
         """
     query_json = {
         "delta": ["adfind-p0002--process_create-windows_any"],
-        "title": "Adfind Enumeration By Trustdmp Command",
+        "title": "Adfind Trustdmp Command",
         "mitre_technique": "t1482",
         "mitre_sub_technique": "",
         "query": query_text
@@ -44,7 +44,7 @@ def adfind_p0003(kql_ago='1d'):
         """
     query_json = {
         "delta": ["adfind-p0003--process_create-windows_any"],
-        "title": "Adfind Enumeration By Dclist Command",
+        "title": "Adfind Dclist Command",
         "mitre_technique": "t1018",
         "mitre_sub_technique": "",
         "query": query_text
@@ -62,7 +62,7 @@ def adfind_p0004(kql_ago='1d'):
         """
     query_json = {
         "delta": ["adfind-p0004--process_create-windows_any"],
-        "title": "Adfind Enumeration By Domainlist Command",
+        "title": "Adfind Domainlist Command",
         "mitre_technique": "",
         "mitre_sub_technique": "",
         "query": query_text
@@ -75,14 +75,12 @@ def adfind_p0005(kql_ago='1d'):
 
     query_text = f"""DeviceProcessEvents
         | where Timestamp >= ago({str(kql_ago)})
-        | where ProcessCommandLine has_any ('pwdnotreqd', 'users', 'computers')
+        | where ProcessCommandLine has 'users_pwdnotreqd'
             and ProcessCommandLine has_any ('-gcb', '-sc')
-        | where ProcessCommandLine contains 'users_pwdnotreqd'
-            or ProcessCommandLine contains 'computers_pwdnotreqd'
         """
     query_json = {
         "delta": ["adfind-p0005--process_create-windows_any"],
-        "title": "Adfind Enumeration By PwdNotReqD Command",
+        "title": "Adfind Users_PwdNotReqD Command",
         "mitre_technique": "",
         "mitre_sub_technique": "",
         "query": query_text
@@ -95,12 +93,13 @@ def adfind_p0006(kql_ago='1d'):
 
     query_text = f"""DeviceProcessEvents
         | where Timestamp >= ago({str(kql_ago)})
-        | where ProcessCommandLine has_all ('objectcategory=computer', '-f')
+        | where ProcessCommandLine has 'computers_pwdnotreqd'
+            and ProcessCommandLine has_any ('-gcb', '-sc')
         """
     query_json = {
         "delta": ["adfind-p0006--process_create-windows_any"],
-        "title": "Adfind Enumeration By ObjectCategory Filter by Computer Command",
-        "mitre_technique": "t1018",
+        "title": "Adfind Computers_PwdNotReqD",
+        "mitre_technique": "",
         "mitre_sub_technique": "",
         "query": query_text
         }
@@ -112,12 +111,12 @@ def adfind_p0007(kql_ago='1d'):
 
     query_text = f"""DeviceProcessEvents
         | where Timestamp >= ago({str(kql_ago)})
-        | where ProcessCommandLine has_all ('objectcategory=person', '-f')
+        | where ProcessCommandLine has_all ('objectcategory=computer', '-f')
         """
     query_json = {
         "delta": ["adfind-p0007--process_create-windows_any"],
-        "title": "Adfind Enumeration By ObjectCategory Filter by Person Command",
-        "mitre_technique": "t1087.002",
+        "title": "Adfind ObjectCategory Filter By Computers Command",
+        "mitre_technique": "t1018",
         "mitre_sub_technique": "",
         "query": query_text
         }
@@ -129,16 +128,32 @@ def adfind_p0008(kql_ago='1d'):
 
     query_text = f"""DeviceProcessEvents
         | where Timestamp >= ago({str(kql_ago)})
+        | where ProcessCommandLine has_all ('objectcategory=person', '-f')
+        """
+    query_json = {
+        "delta": ["adfind-p0008--process_create-windows_any"],
+        "title": "Adfind ObjectCategory Filter By Person Command",
+        "mitre_technique": "t1087.002",
+        "mitre_sub_technique": "",
+        "query": query_text
+        }
+
+    return query_json
+
+
+def adfind_p0009(kql_ago='1d'):
+
+    query_text = f"""DeviceProcessEvents
+        | where Timestamp >= ago({str(kql_ago)})
         | where ProcessCommandLine has_any ('objectcategory=person', 'objectcategory=computer', 'objectcategory=subnet', 
                                             'objectcategory=organizationalUnit', 'domainlist', 'trustdmp', 'adinfo', 
                                             'dclist')
-            and ProcessCommandLine has_any ('-f', '-sc')
         | where ProcessCommandLine contains ' > '
             or ProcessCommandLine contains ' >> '
         """
     query_json = {
-        "delta": ["adfind-p0008--process_create-windows_any"],
-        "title": "Adfind Common Enumeration Command Redirected to Output File",
+        "delta": ["adfind-p0009--process_create-windows_any"],
+        "title": "Adfind Command Output Redirected to File",
         "mitre_technique": "",
         "mitre_sub_technique": "",
         "query": query_text
