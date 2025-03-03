feeeeec0b87cbcf0c8669a654a528436e21a7c9f
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2025-03-03 16:33:53 -0500

    update lsass

--- delta2/detectpy/mde_aht_logic/__init__.py
+++ delta2/detectpy/mde_aht_logic/__init__.py
@@ -2,6 +2,8 @@ from .htool.adfind_logic import (adfind_p0001, adfind_p0002, adfind_p0003, adfin
                                  adfind_p0007, adfind_p0008)
 from .winlol_any.certutil_logic import (certutil_p0001)
 from .winlol_any.comsvcs_logic import (comsvcs_p0001, comsvcs_p0002, comsvcs_p0003, comsvcs_p0004)
+from .winlol_any.lsass import (lsass_p0001, lsass_p0002, lsass_p0003, lsass_p0004, lsass_p0005, lsass_p0006,
+                               lsass_p0007, lsass_p0008)
 from .winlol_any.nltest_logic import (nltest_p0001, nltest_p0002, nltest_p0003, nltest_p0004, nltest_p0005,
                                       nltest_p0006)
 from .winlol_any.rundll_logic import (rundll_p0001, rundll_p0002, rundll_p0003, rundll_p0004)
--- delta2/lib/winlol_any/_shared_references.py
+++ delta2/lib/winlol_any/_shared_references.py
@@ -46,6 +46,77 @@ _certutil = [
 ]
 
 
+_lsass = [
+    {
+        "source_name": "threat hunter playbook",
+        "url": "https://threathunterplaybook.com/hunts/windows/170105-LSASSMemoryReadAccess/notebook.html"
+    },
+    {
+        "source_name": "red canary",
+        "url": "https://redcanary.com/threat-detection-report/techniques/lsass-memory/"
+    },
+    {
+        "source_name": "medium",
+        "url": "https://medium.com/@markmotig/some-ways-to-dump-lsass-exe-c4a75fdc49bf"
+    },
+    {
+        "source_name": "wikipedia",
+        "url": "https://en.wikipedia.org/wiki/Local_Security_Authority_Subsystem_Service"
+    },
+    {
+        "source_name": "deep instinct",
+        "url": "https://www.deepinstinct.com/blog/lsass-memory-dumps-are-stealthier-than-ever-before"
+    },
+    {
+        "source_name": "deep instinct",
+        "url": "https://www.deepinstinct.com/blog/lsass-memory-dumps-are-stealthier-than-ever-before"
+    },
+    {
+        "source_name": "msft",
+        "url": "https://www.microsoft.com/en-us/security/blog/2022/10/05/detecting-and-preventing-lsass-credential-dumping-attacks/"
+    },
+    {
+        "source_name": "msft",
+        "url": "https://learn.microsoft.com/en-us/windows-server/security/credentials-protection-and-management/configuring-additional-lsa-protection"
+    },
+    {
+        "source_name": "msft",
+        "url": "https://learn.microsoft.com/en-us/windows-server/security/windows-authentication/credentials-processes-in-windows-authentication"
+    },
+    {
+        "source_name": "defcpm",
+        "url": "https://media.defcon.org/DEF%20CON%2030/DEF%20CON%2030%20presentations/Asaf%20Gilboa%20-%20LSASS%20Shtinkering%20Abusing%20Windows%20Error%20Reporting%20to%20Dump%20LSASS.pdf"
+    },
+    {
+        "source_name": "deep instinct",
+        "url": "https://www.deepinstinct.com/blog/lsass-memory-dumps-are-stealthier-than-ever-before-part-2"
+    },
+    {
+        "source_name": "detection fyi",
+        "url": "https://detection.fyi/sigmahq/sigma/windows/process_access/proc_access_win_lsass_werfault/"
+    }
+]
+
+
+_nltest = [
+    {
+        "source_name": "mitre attack",
+        "url": "https://attack.mitre.org/software/S0359/"
+    },
+    {
+        "source_name": "msft",
+        "url": "https://learn.microsoft.com/en-us/previous-versions/windows/it-pro/windows-server-2012-r2-and-2012/cc731935(v=ws.11)"
+    },
+    {
+        "source_name": "red canary",
+        "url": "https://redcanary.com/threat-detection-report/techniques/domain-trust-discovery/"
+    },
+    {
+        "source_name": "medium blog",
+        "url": "https://harmj0y.medium.com/a-guide-to-attacking-domain-trusts-ef5f8992bb9d"
+    }
+]
+
 
 _rundll = [
     {
