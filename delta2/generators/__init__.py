564f5c2a732b5bc58054e1c80ea05cc0b9f70df3
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2025-07-06 14:24:06 -0400

    eid update

--- delta2/generators/__init__.py
+++ delta2/generators/__init__.py
@@ -1,3 +1,4 @@
 from delta2.generators.delta_pid import (xls2stx_winlol, xls2stx_htool, xls2stx_pview)
-from delta2.generators.delta_eid import (xls2stx_ransom_process_create)
+from delta2.generators.delta_eid import (xls2stx_ransom_process_create, xls2stx_ransom_process_access,
+                                         xls2stx_ransom_file, xls2stx_ransom_generic)
 
--- delta2/generators/delta_eid.py
+++ delta2/generators/delta_eid.py
@@ -11,12 +11,12 @@ import numpy as np
 lib_e = os.path.join(base_path, 'lib_evidence')
 
 # delta_eid excel sheets
-ransom = os.path.join(lib_e, 'eid-ransom.xlsx')
+ransom_pc = os.path.join(lib_e, 'eid-ransom.xlsx')
 
 
 def xls2stx_ransom_process_create():
     stix_list = []
-    df1 = pd.read_excel(ransom, sheet_name='process_create')
+    df1 = pd.read_excel(ransom_pc, sheet_name='process_create')
     df1 = df1.replace({np.nan: None})
 
     # Add error handling to pinpoint the exact problematic value
@@ -39,7 +39,7 @@ def xls2stx_ransom_process_create():
     for index, row in df1.iterrows():
 
         x = XDeltaEvidenceId(
-            id=x_delta_eid + str(uuid.uuid5(delta_namespace, str(df1.loc[index, 'delta_eid']))),
+            id=x_delta_eid + str(uuid.uuid5(delta_namespace, str(df1.loc[index, 'delta_eid'] + '--process_create'))),
             created_by_ref=delta_identity,
             created=default_timestamp,
             modified=default_timestamp,
@@ -61,7 +61,8 @@ def xls2stx_ransom_process_create():
                 "evidence_type": "Reported",
                 "evidence_date": df1.loc[index, 'eid_date'],
                 "evidence_url": df1.loc[index, 'eid_url'],
-                "tags": df1.loc[index, 'tags']
+                "tags": df1.loc[index, 'tags'],
+                "delta_pid": df1.loc[index, 'delta_pid'],
             }
         )
         stix_list.append(x)
@@ -70,3 +71,166 @@ def xls2stx_ransom_process_create():
     return stix_list
 
 
+
+def xls2stx_ransom_process_access():
+    stix_list = []
+    df1 = pd.read_excel(ransom_pc, sheet_name='process_access')
+    df1 = df1.replace({np.nan: None})
+
+    # Add error handling to pinpoint the exact problematic value
+    def parse_json_safely(json_string):
+        if pd.isna(json_string) or json_string is None:
+            return None  # Or a default empty dictionary, depending on your needs
+        try:
+            return json.loads(json_string)
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON: {e}")
+            print(f"Problematic string: '{json_string}'")
+            return None  # Return None or raise the error if you want to stop execution
+
+    df1['parse_json'] = df1['ns_meta'].apply(parse_json_safely)
+
+    df1['eid_date'] = df1['parse_json'].apply(lambda x: x.get('date', 'N/A'))
+    df1['eid_url'] = df1['parse_json'].apply(lambda x: x.get('url', 'N/A'))
+    df1['tags'] = df1['parse_json'].apply(lambda x: x.get('tags', 'N/A'))
+
+    for index, row in df1.iterrows():
+
+        x = XDeltaEvidenceId(
+            id=x_delta_eid + str(uuid.uuid5(delta_namespace, str(df1.loc[index, 'delta_eid'] + '--process_access'))),
+            created_by_ref=delta_identity,
+            created=default_timestamp,
+            modified=default_timestamp,
+            description=df1.loc[index, 'description'],
+            object_marking_refs=[stix2.TLP_WHITE],
+            labels=[],
+            x_delta_evidence_id=df1.loc[index, 'delta_eid'] + '--process_access',
+            x_evidence_obj={
+                "src_process": df1.loc[index, 'src_process'],
+                "src_process_path": df1.loc[index, 'src_process_path'],
+                "tgt_process": df1.loc[index, 'tgt_process'],
+                "tgt_process_path": df1.loc[index, 'tgt_process_path'],
+                "access": df1.loc[index, 'access'],
+                "trace": df1.loc[index, 'trace'],
+                "object": df1.loc[index, 'object']
+            },
+            x_evidence_meta={
+                "evidence_type": "Reported",
+                "evidence_date": df1.loc[index, 'eid_date'],
+                "evidence_url": df1.loc[index, 'eid_url'],
+                "tags": df1.loc[index, 'tags'],
+                "delta_pid": df1.loc[index, 'delta_pid'],
+            }
+        )
+        stix_list.append(x)
+
+
+    return stix_list
+
+
+def xls2stx_ransom_file():
+    stix_list = []
+    df1 = pd.read_excel(ransom_pc, sheet_name='file_event')
+    df1 = df1.replace({np.nan: None})
+
+    # Add error handling to pinpoint the exact problematic value
+    def parse_json_safely(json_string):
+        if pd.isna(json_string) or json_string is None:
+            return None  # Or a default empty dictionary, depending on your needs
+        try:
+            return json.loads(json_string)
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON: {e}")
+            print(f"Problematic string: '{json_string}'")
+            return None  # Return None or raise the error if you want to stop execution
+
+    df1['parse_json'] = df1['ns_meta'].apply(parse_json_safely)
+
+    df1['eid_date'] = df1['parse_json'].apply(lambda x: x.get('date', 'N/A'))
+    df1['eid_url'] = df1['parse_json'].apply(lambda x: x.get('url', 'N/A'))
+    df1['tags'] = df1['parse_json'].apply(lambda x: x.get('tags', 'N/A'))
+
+    for index, row in df1.iterrows():
+
+        x = XDeltaEvidenceId(
+            id=x_delta_eid + str(uuid.uuid5(delta_namespace, str(df1.loc[index, 'delta_eid'] + '--process_access'))),
+            created_by_ref=delta_identity,
+            created=default_timestamp,
+            modified=default_timestamp,
+            description=df1.loc[index, 'description'],
+            object_marking_refs=[stix2.TLP_WHITE],
+            labels=[],
+            x_delta_evidence_id=df1.loc[index, 'delta_eid'] + '--file_event',
+            x_evidence_obj={
+                "file_action": df1.loc[index, 'file_action'],
+                "file_path": df1.loc[index, 'file_path'],
+                "file_name": df1.loc[index, 'file_name'],
+                "initiating_file_path": df1.loc[index, 'initiating_file_path'],
+                "initiating_file_name": df1.loc[index, 'initiating_file_name'],
+                "initiating_process_cmdline": df1.loc[index, 'initiating_process_cmdline']
+            },
+            x_evidence_meta={
+                "evidence_type": "Reported",
+                "evidence_date": df1.loc[index, 'eid_date'],
+                "evidence_url": df1.loc[index, 'eid_url'],
+                "tags": df1.loc[index, 'tags'],
+                "delta_pid": df1.loc[index, 'delta_pid'],
+            }
+        )
+        stix_list.append(x)
+
+
+    return stix_list
+
+
+def xls2stx_ransom_generic():
+    stix_list = []
+    df1 = pd.read_excel(ransom_pc, sheet_name='generic')
+    df1 = df1.replace({np.nan: None})
+
+    # Add error handling to pinpoint the exact problematic value
+    def parse_json_safely(json_string):
+        if pd.isna(json_string) or json_string is None:
+            return None  # Or a default empty dictionary, depending on your needs
+        try:
+            return json.loads(json_string)
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON: {e}")
+            print(f"Problematic string: '{json_string}'")
+            return None  # Return None or raise the error if you want to stop execution
+
+    df1['parse_json'] = df1['ns_meta'].apply(parse_json_safely)
+
+    df1['eid_date'] = df1['parse_json'].apply(lambda x: x.get('date', 'N/A'))
+    df1['eid_url'] = df1['parse_json'].apply(lambda x: x.get('url', 'N/A'))
+    df1['tags'] = df1['parse_json'].apply(lambda x: x.get('tags', 'N/A'))
+
+    for index, row in df1.iterrows():
+
+        x = XDeltaEvidenceId(
+            id=x_delta_eid + str(uuid.uuid5(delta_namespace, str(df1.loc[index, 'delta_eid'] + '--generic'))),
+            created_by_ref=delta_identity,
+            created=default_timestamp,
+            modified=default_timestamp,
+            description=df1.loc[index, 'description'],
+            object_marking_refs=[stix2.TLP_WHITE],
+            labels=[],
+            x_delta_evidence_id=df1.loc[index, 'delta_eid'] + '--file_event',
+            x_evidence_obj={
+                "htool": df1.loc[index, 'htool'],
+                "hscript": df1.loc[index, 'hscript'],
+                "c2": df1.loc[index, 'c2'],
+                "evidence": df1.loc[index, 'evidence'],
+            },
+            x_evidence_meta={
+                "evidence_type": "Reported",
+                "evidence_date": df1.loc[index, 'eid_date'],
+                "evidence_url": df1.loc[index, 'eid_url'],
+                "tags": df1.loc[index, 'tags'],
+                "delta_pid": df1.loc[index, 'delta_pid'],
+            }
+        )
+        stix_list.append(x)
+
+
+    return stix_list
