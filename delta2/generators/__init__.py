2f1085b25e5b7dcad267ec584b896c2fdbb4f54f
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2025-07-07 12:51:41 -0400

    delta data update

--- delta2/generators/__init__.py
+++ delta2/generators/__init__.py
@@ -2,3 +2,7 @@ from delta2.generators.delta_pid import (xls2stx_winlol, xls2stx_htool, xls2stx_
 from delta2.generators.delta_eid import (xls2stx_ransom_process_create, xls2stx_ransom_process_access,
                                          xls2stx_ransom_file, xls2stx_ransom_generic)
 
+from delta2.generators.delta_did.process_create import (process_create__any, process_create__windows_any,
+                                                        process_create__linux_any, process_create__mac_any,
+                                                        process_create_mde__any)
+
--- delta2/generators/delta_did/process_create.py
+++ delta2/generators/delta_did/process_create.py
@@ -1,4 +1,4 @@
-from delta2.common import (x_delta_did, delta_namespace, delta_identity)
+from delta2.common import (x_delta_did, delta_namespace)
 import delta2.stix as d2s
 import uuid
 
@@ -60,15 +60,15 @@ process_create__linux_any = d2s.XDeltaDataId(
     modified="2025-01-01T00:00:00.000Z",
     name="Process Creation Event From Any Linux OS",
     description="Process creation event from any Linux OS collected by any method.",
-    x_delta_did=_pc_nix_any,
-    x_did_reqs={
+    x_delta_data_id=_pc_nix_any,
+    x_data_reqs={
         "process_cmdline": "Command line used to create the new process or execute image",
         "process_path": "Directory path of process",
         "account": "account associated with process",
         "initiating_process_cmdline": "Command line used to run the process that initiated the event",
         "initiating_process_path": "Directory path of initiating process"
     },
-    x_did_ns_obj={
+    x_data_meta={
         "collection_methods": ["sysmon", "mde", "edr"],
         "operating_system": ["linux"]
     }
@@ -81,15 +81,15 @@ process_create__mac_any = d2s.XDeltaDataId(
     modified="2025-01-01T00:00:00.000Z",
     name="Process Creation Event From Any Mac OS",
     description="Process creation event from any Mac OS collected by any method.",
-    x_delta_did=_pc_mac_any,
-    x_did_reqs={
+    x_delta_data_id=_pc_mac_any,
+    x_data_reqs={
         "process_cmdline": "Command line used to create the new process or execute image",
         "process_path": "Directory path of process",
         "account": "account associated with process",
         "initiating_process_cmdline": "Command line used to run the process that initiated the event",
         "initiating_process_path": "Directory path of initiating process"
     },
-    x_did_ns_obj={
+    x_data_meta={
         "collection_methods": ["mde", "edr"],
         "operating_system": ["mac"]
     }
@@ -101,8 +101,9 @@ process_create_mde__any = d2s.XDeltaDataId(
     created="2020-01-01T00:00:00.000Z",
     modified="2020-01-01T00:00:00.000Z",
     name="Process Creation Event From MSFT Defender Endpoint From Any OS",
-    x_delta_did=_pc_mde_any,
-    x_did_reqs={
+    description="Process creation event from any OS collectedd by MSFT Defender Endpoint.",
+    x_delta_data_id=_pc_mde_any,
+    x_data_reqs={
         "process_cmdline": "Command line used to create the new process or execute image",
         "process_path": "Directory path of process",
         "account": "account associated with process",
@@ -112,7 +113,7 @@ process_create_mde__any = d2s.XDeltaDataId(
         "parent_initiating_process_cmdline": "Command line used to run the  parent process to the initiating process",
         "parent_initiating_process_path": "Directory path of parent process"
     },
-    x_did_ns_obj={
+    x_data_meta={
         "collection_methods": ["mde"],
         "operating_system": ["windows", "linux", "macos"]
     }
